3fcddd30e89b518e7ff86dc5d409924ccb51757f
denchi-45 <hori.takoashi45@gmail.com>
2023-07-05 22:49:40 +0900

    Reviewについての記述を追加しました。

--- src/student.php
+++ src/student.php
@@ -1,8 +1,33 @@
 <?php
+require_once("./Review.php");
+
 class Student {
     private $ids = array(1,2,3,4,5);
+    private $reviews = array();
+    private $studentNo = 1;
+
+    public function __construct(){
+        foreach ( $this ->ids as $id){
+            $this ->reviews[] = new Review();
+        }
+    }
+
     public function subjects() {
         return $this->ids;
+    }
+    public function getReviewText(int $id) {
+        foreach ($this->reviews as $review){
+            if ($review->subjectId == $id){
+                return $review->text;
+            }
+        }
+    }
+    public function setReviewText(int $id, string $text){
+        foreach ($this->reviews as $review){
+            if ($review->subjectId == $id){
+                $review->text = $text;
+            }
+        }
     }
 }
 ?>
