9b79dd19e463dcb5c1a8416f7fc475834e5d25db
ymataku <takuya@outlook.jp>
2023-07-05 22:49:35 +0900

    review作成

--- src/review.php
+++ src/review.php
@@ -2,8 +2,9 @@
 class Review {
     private $subjectid;
     private $text;
-    public function setText(){
-        
+    public function setText($string){
+        $this->text = $string;
+
     }
     public function getText(){
         return $this->text;
