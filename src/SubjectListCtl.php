91f44c56dcbc0c8d30d049eaa6434b756b65c3dc
ymataku <takuya@outlook.jp>
2023-07-06 15:52:00 +0900

    改良

--- src/SubjectListCtl.php
+++ src/SubjectListCtl.php
@@ -17,8 +17,8 @@ class SubjectListCtrl {
     
     foreach($st->subjects() as $s){
       // $sbj[0]->addChild('li', "<a herf=./showreview.html?subject=".$sb->getTitle($s).">".$sb->getTitle($s)."</a>");
-      $sbj[0]->addChild('a', $sb->getTitle($s));
-      $sbj[0]->addAttribute('href',"./showreview.html?subject=".$s);
+      $tmp = $sbj[0]->addChild('a', $sb->getTitle($s));
+      $tmp->addAttribute('href',"./showreview.html?subject=".$s);
 
     }
     echo $doc->asXML();
